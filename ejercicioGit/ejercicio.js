78f0929ed51dc7617ea66e0d81cd3f98992b8848
KRAT0Sss <justinecheverriaggtr@gmail.com>
2025-09-22 21:28:02 -0500

    el boton midifica la imagen.1

--- ejercicioGit/ejercicio.js
+++ ejercicioGit/ejercicio.js
@@ -1,5 +1,6 @@
 saludar=function(){
     let cmpImagenSaludo;
     cmpImagenSaludo=document.getElementById("imgSaludar");
+    cmpImagenSaludo.src="./imagenes/crashYeddie.gif"
     console.log("ingresa a saludar");
 }
