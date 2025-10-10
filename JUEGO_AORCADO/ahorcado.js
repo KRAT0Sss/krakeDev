c355f3e492dc3c185843d54952d1798707e17585
KRAT0Sss <justinecheverriaggtr@gmail.com>
2025-10-10 11:54:38 -0500

    Paso 0

---
+++ JUEGO_AORCADO/ahorcado.js
@@ -0,0 +1,9 @@
+//No se olvide de respirar, mantenga la calma y demuestre lo que sabe
+esMayuscula=function(caracter){
+    let codigo=caracter.charAt(0)
+    if(codigo>=65 && codigo <=90){
+        return true;
+    }else{
+        return false;
+    }
+}
