a3caedf33774b0b92a5f31afd762cb36ae45e8ba
KRAT0Sss <justinecheverriaggtr@gmail.com>
2025-10-10 20:45:37 -0500

    Paso 7

--- JUEGO_AORCADO/ahorcado.js
+++ JUEGO_AORCADO/ahorcado.js
@@ -72,9 +72,9 @@ ingresarLetra = function () {
     if (esMayuscula(letra) == true) {
         validar(letra);
         if (coincidencias == 5) {
-            alert("a ganado");
+            mostrarImagen("ahorcadoImagen","ganador.gif");
         } else if (intentos == 10) {
-            alert("ha perdido")
+            mostrarImagen("ahorcadoImagen","gameOver.gif");
         }
     } else {
         alert("solo se aceptan mayusculas");
@@ -83,23 +83,23 @@ ingresarLetra = function () {
 }
 
 mostrarAhorcado = function () {
-    if(errores==1){
-        mostrarImagen("ahorcadoImagen",Ahorcado_01.png)
-    }else if(errores==2){
-        mostrarImagen("ahorcadoImagen",Ahorcado_02.png)
-    }else if(errores==3){
-        mostrarImagen("ahorcadoImagen",Ahorcado_03.png)
-    }else if(errores==4){
-        mostrarImagen("ahorcadoImagen",Ahorcado_04.png)
-    }else if(errores==5){
-        mostrarImagen("ahorcadoImagen",Ahorcado_05.png)
-    }else if(errores==6){
-        mostrarImagen("ahorcadoImagen",Ahorcado_06.png)
-    }else if(errores==7){
-        mostrarImagen("ahorcadoImagen",Ahorcado_07.png)
-    }else if(errores==8){
-        mostrarImagen("ahorcadoImagen",Ahorcado_08.png)
-    }else if(errores==9){
-        mostrarImagen("ahorcadoImagen",Ahorcado_09.png)
-    }
+   if (errores == 1) {
+    mostrarImagen("ahorcadoImagen", "Ahorcado_01.png");
+} else if (errores == 2) {
+    mostrarImagen("ahorcadoImagen", "Ahorcado_02.png");
+} else if (errores == 3) {
+    mostrarImagen("ahorcadoImagen", "Ahorcado_03.png");
+} else if (errores == 4) {
+    mostrarImagen("ahorcadoImagen", "Ahorcado_04.png");
+} else if (errores == 5) {
+    mostrarImagen("ahorcadoImagen", "Ahorcado_05.png");
+} else if (errores == 6) {
+    mostrarImagen("ahorcadoImagen", "Ahorcado_06.png");
+} else if (errores == 7) {
+    mostrarImagen("ahorcadoImagen", "Ahorcado_07.png");
+} else if (errores == 8) {
+    mostrarImagen("ahorcadoImagen", "Ahorcado_08.png");
+} else if (errores == 9) {
+    mostrarImagen("ahorcadoImagen", "Ahorcado_09.png");
+}
 }
