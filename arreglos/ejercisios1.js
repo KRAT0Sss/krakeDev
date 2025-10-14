4b3df9b6c859da826e7edf2df7f1a1f3fa1189d5
KRAT0Sss <justinecheverriaggtr@gmail.com>
2025-10-13 21:34:38 -0500

    Arreglos y tablas

--- arreglos/ejercisios1.js
+++ arreglos/ejercisios1.js
@@ -23,6 +23,7 @@ probarAgregar = function () {
 
 agregarNotas = function (nota) {
     notas.push(nota);
+    mostrarNotas();
 }
 
 calcularPromedio = function () {
@@ -38,4 +39,25 @@ calcularPromedio = function () {
 ejecutarPromedio = function () {
     let promedio = calcularPromedio();
     mostrarTexto("lblPromedio", promedio);
+}
+
+generarTabla = function () {
+    let contenidoTabla = "";
+    let cmpTabla = document.getElementById("divTabla");
+    contenidoTabla += "<table><tr><td>UNO</td></tr>"+"<tr><td>DOS</td></tr></table>";
+    cmpTabla.innerHTML=contenidoTabla;
+}
+
+mostrarNotas = function () {
+    let cmpTabla = document.getElementById("divTabla");
+    let contenidoTabla="<table><tr><th>NOTAS</th></tr>";
+    let miNota;
+    for (let i=0;i<notas.length;i++){
+        miNota=notas[i];//saca cada nota ingresada
+        contenidoTabla+="<tr><td>";
+        contenidoTabla+=miNota;
+        contenidoTabla+="</tr></td>";
+    }
+    contenidoTabla+= "</table>";
+    cmpTabla.innerHTML=contenidoTabla;
 }
