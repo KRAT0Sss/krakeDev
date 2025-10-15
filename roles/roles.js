71c51f8bfc72c733313e1a38918960c91a377514
KRAT0Sss <justinecheverriaggtr@gmail.com>
2025-10-14 22:12:47 -0500

    roles, mostrar div empleado

---
+++ roles/roles.js
@@ -0,0 +1,19 @@
+let empleados = [
+    { cedula: "1714616123", nombre: "John", apellido: "Cena", sueldo: 500.0 },
+    { cedula: "0914632123", nombre: "Luisa", apellido: "Gonzalez", sueldo: 900.0 }
+]
+
+mostrarOpcionEmpleado = function () {
+    ocultarComponente("divRol");
+    ocultarComponente("divResumen");
+}
+
+mostrarOpcionRol = function () {
+    ocultarComponente("divEmpleado");
+    ocultarComponente("divResumen");
+}
+
+mostrarOpcionResumen = function () {
+    ocultarComponente("divEmpleado");
+    ocultarComponente("divRol");
+}
